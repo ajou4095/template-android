a4bb2a12ea0e7fa9227fddc7f13aea89a5abe241
AjouShama <ajoushama@gmail.com>
2022-03-31 23:58:50 +0900

    [Util] Event, BundleUtil, LiveDataUtil - https://medium.com/prnd/mvvm%EC%9D%98-viewmodel%EC%97%90%EC%84%9C-%EC%9D%B4%EB%B2%A4%ED%8A%B8%EB%A5%BC-%EC%B2%98%EB%A6%AC%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95-6%EA%B0%80%EC%A7%80-31bb183a88ce

--- app/src/main/java/com/ray/template/common/util/BundleUtil.kt
+++ app/src/main/java/com/ray/template/common/util/BundleUtil.kt
@@ -2,11 +2,39 @@ package com.ray.template.common.util
 
 import androidx.lifecycle.SavedStateHandle
 
+fun SavedStateHandle.getBooleanOrDefault(
+    key: String,
+    defaultValue: Boolean = false
+): Boolean {
+    return get<Boolean>(key) ?: defaultValue
+}
+
 fun SavedStateHandle.getByteOrDefault(
     key: String,
     defaultValue: Byte = 0
 ): Byte {
     return get<Byte>(key) ?: defaultValue
+}
+
+fun SavedStateHandle.getCharOrDefault(
+    key: String,
+    defaultValue: Char = Char.MIN_VALUE
+): Char {
+    return get<Char>(key) ?: defaultValue
+}
+
+fun SavedStateHandle.getDoubleOrDefault(
+    key: String,
+    defaultValue: Double = 0.0
+): Double {
+    return get<Double>(key) ?: defaultValue
+}
+
+fun SavedStateHandle.getFloatOrDefault(
+    key: String,
+    defaultValue: Float = 0f
+): Float {
+    return get<Float>(key) ?: defaultValue
 }
 
 fun SavedStateHandle.getIntOrDefault(
@@ -16,11 +44,18 @@ fun SavedStateHandle.getIntOrDefault(
     return get<Int>(key) ?: defaultValue
 }
 
-fun SavedStateHandle.getFloatOrDefault(
+fun SavedStateHandle.getLongOrDefault(
     key: String,
-    defaultValue: Float = 0f
-): Float {
-    return get<Float>(key) ?: defaultValue
+    defaultValue: Long = 0L
+): Long {
+    return get<Long>(key) ?: defaultValue
+}
+
+fun SavedStateHandle.getShortOrDefault(
+    key: String,
+    defaultValue: Short = 0
+): Short {
+    return get<Short>(key) ?: defaultValue
 }
 
 fun SavedStateHandle.getStringOrDefault(
--- app/src/main/java/com/ray/template/common/util/LiveDataUtil.kt
+++ app/src/main/java/com/ray/template/common/util/LiveDataUtil.kt
@@ -1,1 +1,30 @@
 package com.ray.template.common.util
+
+import androidx.lifecycle.LiveData
+
+val LiveData<Boolean>.valueOrDefault: Boolean
+    get() = value ?: false
+
+val LiveData<Byte>.valueOrDefault: Byte
+    get() = value ?: 0
+
+val LiveData<Char>.valueOrDefault: Char
+    get() = value ?: Char.MIN_VALUE
+
+val LiveData<Double>.valueOrDefault: Double
+    get() = value ?: 0.0
+
+val LiveData<Float>.valueOrDefault: Float
+    get() = value ?: 0f
+
+val LiveData<Int>.valueOrDefault: Int
+    get() = value ?: 0
+
+val LiveData<Long>.valueOrDefault: Long
+    get() = value ?: 0L
+
+val LiveData<Short>.valueOrDefault: Short
+    get() = value ?: 0
+
+val LiveData<String>.valueOrDefault: String
+    get() = value ?: ""
