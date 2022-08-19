d7e35656a230a9177f0a1b1e2ee90267545f142b
AjouShama <ajoushama@gmail.com>
2022-08-20 00:51:56 +0900

    [Gradle] project build.gradle Kotlin DSL 적용

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,33 @@
+object Versions {
+    // sdk
+    const val minSdkVersion = 23
+    const val targetSdkVersion = 32
+    const val compileSdkVersion = 32
+
+    // version
+    const val versionCode = 1
+    const val versionName = "1.0"
+
+    // dependencies
+    const val hiltVersion = "2.41"
+    const val roomVersion = "2.4.2"
+    const val retrofitVersion = "2.9.0"
+    const val retrofitCoroutineAdapterVersion = "0.9.2"
+    const val sandwichVersion = "1.2.4"
+    const val moshiVersion = "1.13.0"
+    const val lifecycleVersion = "2.4.1"
+    const val glideVersion = "4.12.0"
+    const val coroutineVersion = "1.6.0"
+    const val tedPermissionVersion = "3.3.0"
+    const val lottieVersion = "4.2.2"
+    const val timberVersion = "5.0.1"
+    const val flipperVersion = "0.139.0"
+    const val flipperSoLoaderVersion = "0.10.3"
+    const val leakcanaryVersion = "2.8.1"
+    const val androidXCoreVersion = "1.7.0"
+    const val androidXAppCompatVersion = "1.4.1"
+    const val androidXConstraintLayoutVersion = "2.1.3"
+    const val androidXFragmentVersion = "1.4.1"
+    const val androidMaterialVersion = "1.5.0"
+
+}
