d8cbd8c4526c4fb0d0eb80255c34802049b1652f
Ray Jang <ajou4095@gmail.com>
2024-02-19 19:12:47 +0900

    [App] Firebase Initialize

--- app/src/main/kotlin/com/ray/template/TemplateApplication.kt
+++ app/src/main/kotlin/com/ray/template/TemplateApplication.kt
@@ -1,7 +1,16 @@
 package com.ray.template
 
 import android.app.Application
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-open class TemplateApplication : Application()
+open class TemplateApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Firebase Initialize
+        Firebase.analytics
+    }
+}
