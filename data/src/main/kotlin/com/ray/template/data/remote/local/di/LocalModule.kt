de2b73ddd19719522ef07a21eb3b19f8db6b1f9e
Ray Jang <ajou4095@gmail.com>
2024-02-19 16:09:59 +0900

    [Data] Room Database

--- data/src/main/kotlin/com/ray/template/data/remote/local/di/LocalModule.kt
+++ data/src/main/kotlin/com/ray/template/data/remote/local/di/LocalModule.kt
@@ -1,7 +1,10 @@
 package com.ray.template.data.remote.local.di
 
 import android.content.Context
+import androidx.room.Room
 import com.ray.template.data.remote.local.SharedPreferencesManager
+import com.ray.template.data.remote.local.database.TemplateDatabase
+import com.ray.template.data.remote.local.database.sample.SampleDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,5 +22,25 @@ internal object LocalModule {
         @ApplicationContext context: Context
     ): SharedPreferencesManager {
         return SharedPreferencesManager(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTemplateDatabase(
+        @ApplicationContext context: Context
+    ): TemplateDatabase {
+        return Room.databaseBuilder(
+            context,
+            TemplateDatabase::class.java,
+            TemplateDatabase.DATABASE_NAME
+        ).fallbackToDestructiveMigration().build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideSampleDao(
+        templateDatabase: TemplateDatabase
+    ): SampleDao {
+        return templateDatabase.sampleDao()
     }
 }
